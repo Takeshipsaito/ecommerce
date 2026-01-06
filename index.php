fb7fda2cc94d34deb8b9e58fa79172ae6b57a685
Takeshi Saito <takeshipereira25@gmail.com>
2026-01-06 17:29:42 -0300

    criamos pageadmin e tpl admin

--- index.php
+++ index.php
@@ -3,14 +3,22 @@ require_once("vendor/autoload.php");
 
 use Slim\Slim;
 use Hcode\Page;
+use Hcode\pageadmin;
 
 $app = new Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
     echo "";
-    $page = new Page();
+    $page = new page();
+    $page->setTpl("index");
+});
+
+$app->get('/admin', function() {
+    echo "";
+    $page = new pageadmin();
     $page->setTpl("index");
 });
 
 $app->run();
+?>
