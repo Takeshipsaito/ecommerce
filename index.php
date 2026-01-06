f3a69f0a049746fa7595384e83dc9637c2dd954b
Takeshi Saito <takeshipereira25@gmail.com>
2026-01-06 15:09:36 -0300

    autoload hcodebr classes

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo "ECOMMERCE FUNCIONANDO";
+<?php 
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+$app->config('debug', true);
+
+$app->get('/', function() {
+    $sql = new Hcode\DB\Sql();
+    // ATENÇÃO: Adicione "$results =" antes da chamada do select
+    $results = $sql->select("SELECT * FROM tb_users");
+    
+    echo json_encode($results);
+});
+
+$app->run();
 ?>
